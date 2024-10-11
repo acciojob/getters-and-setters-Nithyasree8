31c64ebc9de45916693ec9261fed613eb2f8a79c
Nithyasree8 <113105796+Nithyasree8@users.noreply.github.com>
2024-10-11 20:11:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 //complete this code
-class Person {}
+class Person {
+    constructor(name, age) {
+        this._name = name; // Use a private variable to store the name
+        this._age = age;   // Use a private variable to store the age
+    }
 
-class Student extends Person {}
+    get name() {
+        return this._name; // Getter for name
+    }
 
-class Teacher extends Person {}
+    set age(age) {
+        this._age = age; // Setter for age
+    }
+
+    get age() {
+        return this._age; // Getter for age (optional, but useful)
+    }
+}
+
+class Student extends Person {
+    study() {
+        console.log(`${this.name} is studying`); // Method to log studying message
+    }
+}
+
+class Teacher extends Person {
+    teach() {
+        console.log(`${this.name} is teaching`); // Method to log teaching message
+    }
+}
 
 // Do not change the code below this line
 window.Person = Person;
